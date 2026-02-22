17cec1002d4cc2b7b24bbcd749da532db7406ee4
Venkata Ratnam Bhumireddy <58448762+venkataratnamb20@users.noreply.github.com>
2026-02-22 01:57:03 +0000

    run bash scri[t

---
+++ actions2602/welcome.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "Welcome to Actions 2602! This is a sample workflow that demonstrates how to use GitHub Actions to automate your software development processes. You can customize this workflow to fit your specific needs and requirements. Let's get started!"
