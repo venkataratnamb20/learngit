ea23311bc49e33808f182a947f559ae848b126b6
Venkata Ratnam Bhumireddy <58448762+venkataratnamb20@users.noreply.github.com>
2026-02-23 03:31:36 +0000

    run python script
    print current pwd and list files and dirs.

--- actions2602/basics.py
+++ actions2602/basics.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 
+import sys
 import os
-print(os.__version__)
+print(sys.executable)
 
 def main():
     print("Hello World!")
+    print(f'PWD: {os.getcwd()}')
+    print('files:')
+    print(os.listdir("."))
 
 
 if __name__ == "__main__":
